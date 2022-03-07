4ea611a6213b2fa0515d381b6aebe15633efa678
ViruSave <savevirusx@example.com>
2022-03-07 11:39:57 +0700

    updateWeb25

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('avatar')->default('https://res.cloudinary.com/asteriks/image/upload/v1646627093/an3szlmxb0cqlg9lainu.png');
+            $table->string('avatar')->default('{{ URL(https://res.cloudinary.com/asteriks/image/upload/v1646627203/dyiiak6i4zcm3ubutskf.png) }}');
             $table->string('role');
             $table->tinyInteger('isban')->default('0');
             $table->rememberToken();
